74aee40dac5dc9e34886a3b7ac77e56b6fb63b78
osama rawas <osama@shamlogix.tech>
2026-06-09 12:46:22 +0300

    Update database configuration: change default DB_PORT from 5432 to 5435

--- config/settings.py
+++ config/settings.py
@@ -99,7 +99,7 @@ DATABASES = {
         "USER": config("DB_USER"),
         "PASSWORD": config("DB_PASSWORD"),
         "HOST": config("DB_HOST", default="localhost"),
-        "PORT": config("DB_PORT", default="5432"),
+        "PORT": config("DB_PORT", default="5435"),
     }
 }
 
